a9dce362d60221c7696f36ec43cb70828b86742d
kondo takeshi <take.she12@gmail.com>
2018-05-21 15:59:34 +0900

    add body-fat

--- graph.py
+++ graph.py
@@ -6,13 +6,23 @@ import datetime as dt
 
 plt.style.use('ggplot') 
 
-df = pd.read_csv('Weight Data.csv',header=None, skiprows=2,index_col=0,names=['datetime', 'weight'])
-df.index = pd.to_datetime(df.index)
+dfw = pd.read_csv('Weight Data.csv',header=None, skiprows=2,index_col=0,names=['datetime', 'weight'])
+dfw.index = pd.to_datetime(dfw.index)
 
-plt.plot(df.index, df['weight'])
+plt.plot(dfw.index, dfw['weight'])
 plt.xticks(rotation=70)
 plt.ylim([65,75])
 plt.title("Weight")
 plt.xlabel("Datetime")
 plt.ylabel("Weight(kg)")
 plt.savefig("weight.png")
+
+dfw = pd.read_csv('Body Fat Percentage Data.csv',header=None, skiprows=2,index_col=0,names=['datetime', 'fat'])
+dfw.index = pd.to_datetime(dfw.index)
+
+plt.plot(dfw.index, dfw['fat'])
+plt.ylim([0.1,0.3])
+plt.title("Body Fat")
+plt.ylabel("Body Fat")
+plt.savefig("fat.png")
+
